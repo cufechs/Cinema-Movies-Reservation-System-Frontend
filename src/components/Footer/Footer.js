dcce8bc3f25b891f8db2793428d5291a9d7fbc4b
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-12-18 17:02:37 +0200

    added header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,68 @@
-import React from 'react';
+// import React from 'react';
+// import './Footer.css';
 
-const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+// const Footer = () => {
+//     return (
+//         <div className="footer">
+//             Footer
+//         </div>
+//     )
+// }
+
+// export default Footer;
+
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import './Footer.css';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Footer;
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '20vh',
+      }}
+    >
+      
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box> 
+  )
+}
+
+  
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import './Header.css';
 
 const Header = () => {
+    // TODO: check if user is logged in, conditional rendering on login signup div
+
     return (
-        <div>
-            Header
+        <div className="header">
+            <NavLink to="/">
+                <div className="site__logo">
+                    Cinema Movies Reservation
+                </div>
+            </NavLink>
+            <Stack spacing={2} direction="row">
+                <NavLink to="/login">
+                    <Button variant="outlined" style={{color: 'white'}}>LOGIN</Button>
+                </NavLink>
+                <NavLink to="/signup">
+                    <Button variant="outlined" style={{color: 'white'}}>SIGN UP</Button>
+                </NavLink>
+            </Stack>
         </div>
     )
 }
