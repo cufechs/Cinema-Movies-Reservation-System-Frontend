de07cce8d9631f330d17f36c5c20a8e2cea152cc
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-12-25 19:16:55 +0200

    propagating screentime state to parent component

--- src/components/ScreenTimes/ScreenTimes.js
+++ src/components/ScreenTimes/ScreenTimes.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ScreenTimes.css';
 
-const ScreenTimes = () => {
+const ScreenTimes = (props) => {
+
     return (
         <div className="screen__times">
             <ol >
-                <ScreenTime time="1:30" occupied={false}/>
-                <ScreenTime time="2:30" occupied={false}/>
-                <ScreenTime time="3:30" occupied={false}/>
-                <ScreenTime time="4:30" occupied={false}/>
-                <ScreenTime time="5:30" occupied={true}/>
+                <ScreenTime time="1:30" occupied={false} setSelectedScreenTime={props.setSelectedTime}/>
+                <ScreenTime time="2:30" occupied={false} setSelectedScreenTime={props.setSelectedTime}/>
+                <ScreenTime time="3:30" occupied={false} setSelectedScreenTime={props.setSelectedTime}/>
+                <ScreenTime time="4:30" occupied={false} setSelectedScreenTime={props.setSelectedTime}/>
+                <ScreenTime time="5:30" occupied={true} setSelectedScreenTime={props.setSelectedTime}/>
             </ol>
         </div>
     )
 }
 
 const ScreenTime = (props) => {
+
+    const handleSelection = () => {
+        props.setSelectedScreenTime(props.time);
+    }
+
     return (
         <li className="list__item">
-            <a className={`${'action'} ${props.occupied ? 'occupied' : 'showtime'} `} >{props.time}pm</a>
+            <a className={`${'action'} ${props.occupied ? 'occupied' : 'showtime'} `} onClick={handleSelection} >{props.time}pm</a>
         </li>
     )
 }
